8511724143d66cf0e472ec9612d0777b981cc463
Wang Guan <momocraft@gmail.com>
2021-10-23 12:13:05 +0900

    revise UI

--- client-web/pages/message-sink/[sinkName].tsx
+++ client-web/pages/message-sink/[sinkName].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { Button, Heading } from '@chakra-ui/react';
 import { MessageSinkDemo } from '../../src/message-sink/message-sink';
+import { DefaultMeta } from '../../src/components/meta/default-meta';
 
 const MessageSinkShowPage: React.FC = () => {
   const router = useRouter();
@@ -9,12 +10,14 @@ const MessageSinkShowPage: React.FC = () => {
 
   return (
     <div className="max-w-screen-sm mx-auto pt-24">
+      <DefaultMeta title={sinkName ? `${sinkName} | Message Sink DEMO` : `Message Sink Demo`} />
       <div className="h-24">
         <Heading as="h1" className="text-center " fontSize="xl">
           DEMO: message-sink API
+          <span className="text-sm ml-2">(open Developer Console to see messages)</span>
         </Heading>
-        <div className="text-right">
-          <Button size="sm" onClick={() => router.push('/message-sink')}>
+        <div className="mt-4 text-right">
+          <Button className="ml-4" size="sm" onClick={() => router.push('/message-sink')}>
             BACK
           </Button>
         </div>
--- client-web/pages/message-sink/index.tsx
+++ client-web/pages/message-sink/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { MessageSinkPicker } from '../../src/message-sink/message-sink';
 import { useRouter } from 'next/router';
 import { Heading } from '@chakra-ui/react';
+import {DefaultMeta} from "../../src/components/meta/default-meta";
 
 const MessageSinkIndexPage: React.FC = () => {
   const router = useRouter();
   return (
     <div className="max-w-screen-sm mx-auto pt-24">
+      <DefaultMeta title="Message Sink DEMO" />
       <div className="h-24">
         <Heading as="h1" fontSize="xl" className="text-center">
           DEMO: message-sink API
