cba9427a9c7035729010f47b48631b77896b2957
Wang Guan <momocraft@gmail.com>
2021-11-07 01:48:04 +0900

    regenerate openapi client code

---
+++ client-web/src/generated/todo-api/models/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BadRequest';
+export * from './NotFound';
+export * from './NotImplemented';
+export * from './ServerError';
+export * from './Todo';
+export * from './TodoCreateRequest';
+export * from './Unauthorized';
--- scripts/openapi-codegen.sh
+++ scripts/openapi-codegen.sh
@@ -6,27 +6,23 @@ set -o pipefail
 cd $(dirname "$0")
 SCRIPT_DIR=$(dirname "$0")
 SPEC_DIR="$PWD/../api-spec"
+CLIENT_ROOT="$PWD/../client-web"
 
-save-spec () {
-  pushd $SCRIPT_DIR/../server-openapi
-  export WRITE_OPENAPI_SPEC_TO_FILE="$SPEC_DIR/todo-api.yaml"
-  sbt "run writeOpenApiSpec"
+generate-web () {
+  local API_ID="$1"
+  local GENERATED_ROOT="$PWD/../client-web/src/generated/$API_ID"
+  rm -rf $GENERATED_ROOT && mkdir -pv $GENERATED_ROOT
+
+  yarn openapi-generator-cli generate \
+      -g typescript-fetch             \
+      -i "$SPEC_DIR/$API_ID.yaml"    \
+      -c ./ts-fetch-options.json      \
+      -o "$GENERATED_ROOT"
+
+  pushd "$CLIENT_ROOT"
+  yarn prettier --write "$GENERATED_ROOT"
   popd
 }
 
-generate-web () {
-  local GENERATED_ROOT="$PWD/../client-web/src/generated"
-  rm -rf $GENERATED_ROOT/openapi* && mkdir -pv $GENERATED_ROOT
-
-  yarn openapi-generator-cli generate    \
-      -g typescript-fetch                \
-      -i "$SPEC_DIR/todo-api.yaml" \
-      -c ./ts-fetch-options.json         \
-      -o $GENERATED_ROOT/openapi-fetch
-
-  yarn prettier --write "$GENERATED_ROOT"/openapi*
-}
-
 yarn
-save-spec
-generate-web
+generate-web "todo-api"
