33b822d31f31a37b16b0d7392baf01fb8c5758ab
Wang Guan <momocraft@gmail.com>
2021-11-14 18:35:08 +0900

    rewrite TodoAppV1

--- client-web/pages/todo-list/index.tsx
+++ client-web/pages/todo-list/index.tsx
@@ -6,7 +6,7 @@ const TodoListAppPage: React.FC = () => {
   return (
     <div>
       <DefaultMeta title="todo list" />
-      <div className="w-screen-md mx-auto my-4">
+      <div className="max-w-screen-sm mx-auto my-4 px-2 md:px-0">
         <h1 className="text-center">Todo List</h1>
         <TodoAppV1 apiOrigin="http://127.0.0.1:8080/stateless-openapi/todos" />
       </div>
--- client-web/src/todo-app-v1/todo-app-v1.tsx
+++ client-web/src/todo-app-v1/todo-app-v1.tsx
@@ -1,75 +1,138 @@
 import { DeleteTODORequest, Todo, CreateTODORequest, UpdateTODORequest } from './generated';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { callTodoApi } from './todo-api';
+import { Button, Checkbox } from '@chakra-ui/react';
+import { array, date, ord } from 'fp-ts';
 
 export const TodoAppV1: React.FC<{ apiOrigin: string }> = (props) => {
-  const [state, setState] = useState<null | { mode: 'list'; items: Todo[] } | { mode: 'show'; item: Todo }>(null);
+  const [state, setState] = useState<null | { mode: 'list'; items: Todo[] }>(null);
 
-  const onList = useCallback(async () => {
+  const reloadTodos = useCallback(async () => {
     setState(null);
     const items = await callTodoApi(props.apiOrigin, (api) => api.listTODO());
     setState({ mode: 'list', items: items.todos ?? [] });
   }, [props.apiOrigin]);
 
-  const onShow = async (todoId: number) => {
-    setState(null);
-    const item = await callTodoApi(props.apiOrigin, (api) => api.showTODO({ todoId }));
-    setState({ mode: 'show', item });
-  };
-
   const onCreate = async (req: CreateTODORequest) => {
-    await callTodoApi(props.apiOrigin, (api) => api.createTODO(req));
-    await onList();
+    const created = await callTodoApi(props.apiOrigin, (api) => api.createTODO(req));
+    // setState((_state) => (_state?.mode === 'list' ? { mode: 'list', items: [..._state.items, created] } : null));
+    await reloadTodos();
   };
 
   const onUpdate = async (req: UpdateTODORequest) => {
-    await callTodoApi(props.apiOrigin, (api) => api.updateTODO(req));
-    await onList();
+    const updated = await callTodoApi(props.apiOrigin, (api) => api.updateTODO(req));
+    await reloadTodos();
   };
 
   const onDelete = async (req: DeleteTODORequest) => {
     await callTodoApi(props.apiOrigin, (api) => api.deleteTODO(req));
-    await onList();
+    await reloadTodos();
   };
 
   useEffect(() => {
-    setTimeout(onList);
-  }, [onList, props.apiOrigin]);
+    setTimeout(reloadTodos);
+  }, [reloadTodos, props.apiOrigin]);
 
-  return state?.mode === 'list' ? (
-    <TodoList items={state.items} onSave={onUpdate} />
-  ) : state?.mode === 'show' ? null : null;
-};
+  const sortedItems = useMemo(
+    () => state?.items.sort((a, b) => date.Ord.compare(a.updatedAt, b.updatedAt)).reverse() || [],
+    [state],
+  );
 
-const TodoShow: React.FC<{
-  item: Todo;
-  onSave?(req: UpdateTODORequest): void;
-  onDelete?(req: DeleteTODORequest): void;
-}> = (props) => {
   return (
     <div>
-      <label>Title</label>
-      <h5>{props.item.title}</h5>
+      <TodoDraft onCreate={onCreate} />
+      <hr className="my-1" />
+      <div className="text-center mb-1">
+        <Button isDisabled={!state} onClick={reloadTodos} backgroundColor="blue.500" color="white" width="md">
+          Reload
+        </Button>
+      </div>
+      <TodoList items={sortedItems} onUpdate={onUpdate} onDelete={onDelete} onReload={reloadTodos} />
     </div>
   );
 };
 
 const TodoList: React.FC<{
   items: Todo[];
-  onSave?(req: UpdateTODORequest): void;
+  onUpdate?(req: UpdateTODORequest): void;
   onDelete?(req: DeleteTODORequest): void;
+  onReload?(): void;
 }> = (props) => (
-  <div>
-    <ul>
-      {props.items.map((item) => (
-        <li key={item.id}>
-          <TodoShow item={item} />
-        </li>
-      ))}
-    </ul>
-  </div>
+  <ul className="space-y-2">
+    {props.items.map((item) => (
+      <li key={item.id}>
+        <TodoShow item={item} onDelete={props.onDelete} onUpdate={props.onUpdate} />
+        <hr className="mt-1" />
+      </li>
+    ))}
+  </ul>
 );
 
 const TodoDraft: React.FC<{ onCreate?(req: CreateTODORequest): void }> = (props) => {
-  return null;
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const onCreate = () => {
+    props.onCreate?.({ createTodoIntent: { title, desc: description } });
+    setTitle('');
+    setDescription('');
+  };
+  const isFilled = title.length > 0 && description.length > 0;
+  return (
+    <div>
+      <div className="flex items-baseline ">
+        <label className="w-24">Title</label>
+        <input value={title} onChange={(ev) => setTitle(ev.target.value)} className="flex-1 border px-1" />
+      </div>
+      <div className="flex mt-1 items-baseline">
+        <label className="w-24">Description</label>
+        <input value={description} onChange={(ev) => setDescription(ev.target.value)} className="flex-1 border px-1" />
+      </div>
+      <div className="mt-1 text-right">
+        <Button isDisabled={!isFilled} onClick={onCreate} backgroundColor="blue.500" color="white" size="sm">
+          Create
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+const TodoShow: React.FC<{
+  item: Todo;
+  onUpdate?(req: UpdateTODORequest): void;
+  onDelete?(req: DeleteTODORequest): void;
+}> = (props) => {
+  const [title, setTitle] = useState(props.item.title);
+  const [desc, setDesc] = useState(props.item.desc);
+  const [finished, setFinished] = useState(props.item.finished);
+  const onSave = () => {
+    props.onUpdate?.({ todoId: props.item.id, todo: { ...props.item, title, desc, finished } });
+  };
+  const onDelete = () => {
+    props.onDelete?.({ todoId: props.item.id });
+  };
+  const isDirty = title !== props.item.title || desc !== props.item.desc || finished !== props.item.finished;
+
+  return (
+    <div>
+      <div className="flex items-baseline ">
+        <label className="w-24">Title</label>
+        <input value={title} onChange={(ev) => setTitle(ev.target.value)} className="flex-1 border px-1" />
+      </div>
+      <div className="flex mt-1 items-baseline">
+        <label className="w-24">Description</label>
+        <input value={desc} onChange={(ev) => setDesc(ev.target.value)} className="flex-1 border px-1" />
+      </div>
+      <div className="flex mt-1 items-center">
+        <label className="w-24">Finished</label>
+        <Checkbox isChecked={finished} onChange={(ev) => setFinished(ev.target.checked)} className="" />
+        <span className="flex-1" />
+        <Button isDisabled={!isDirty} onClick={onSave} backgroundColor="blue.500" color="white" size="sm">
+          Update
+        </Button>
+        <Button onClick={onDelete} backgroundColor="yellow.500" color="white" size="sm" className="ml-4">
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
 };
