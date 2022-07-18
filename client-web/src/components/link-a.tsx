11aaed8210f52411c5ae48cf47070245cc676143
Wang Guan <momocraft@gmail.com>
2022-07-19 02:37:39 +0900

    fix nextjs warnings

--- client-web/src/components/link-a.tsx
+++ client-web/src/components/link-a.tsx
@@ -1,10 +1,10 @@
-import { Link } from '@chakra-ui/react';
+import Link from 'next/link';
 import React, { AnchorHTMLAttributes } from 'react';
 
 export const LinkA: React.FC<{ isExternal?: boolean } & AnchorHTMLAttributes<HTMLAnchorElement>> = (props) => {
   const { href, children, isExternal, ...rest } = props;
   return (
-    <Link href={props.href} isExternal={isExternal}>
+    <Link href={props.href!} passHref>
       <a href={props.href} {...rest}>
         {children}
       </a>
--- client-web/src/components/meta/default-meta.tsx
+++ client-web/src/components/meta/default-meta.tsx
@@ -6,26 +6,6 @@ export const DefaultMeta: React.FC<{ title?: string }> = (props) => {
   return (
     <Head>
       <title key="head-title">{props.title ?? 'untitled'}</title>
-      <link
-        rel="stylesheet"
-        key="css-tailwindcss"
-        href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.2/dist/tailwind.min.css"
-        integrity="sha256-KwBcfPYYUP4pXG0aiIA8nTSuAqRzRWdtoHQktxvMVf4="
-        crossOrigin="anonymous"
-      />
-
-      <link
-        key="css-fontawesome4"
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css"
-        integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0="
-        crossOrigin="anonymous"
-      />
-      <meta
-        key="meta-viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1,maximum-scale=1.5,minimum-scale=1"
-      />
     </Head>
   );
 };
